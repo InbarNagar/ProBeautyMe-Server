8ae111ad0585e0883fdeb93330253eaf30efb50c
OfirBidani <117892338+OfirBidani@users.noreply.github.com>
2023-05-12 15:00:36 +0300

    temp

---
+++ BeautyMeWEB/DTO/AppointmentDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeautyMeWEB.DTO
+{
+    public class AppointmentDTO
+    {
+        public int Number_appointment;
+        public System.DateTime Date;
+        public System.TimeSpan Start_time;
+        public System.TimeSpan End_time;
+        public string Is_client_house;
+        public int Business_Number;
+        public string Appointment_status;
+        public string AddressStreet;
+        public string AddressCity;
+        public string AddressHouseNumber;
+        public string BusinessName;//business name
+        public string ID_Client;
+
+    }
+}
+
+
